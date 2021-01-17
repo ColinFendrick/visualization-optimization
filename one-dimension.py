ba5f9975e18c02300ccfc692774c14ca9b93fb45
Colin Fendrick <colin.fendrick@gmail.com>
2021-01-17 11:31:17 -0800

    Add the basics on one-dimension

--- one-dimension.py
+++ one-dimension.py
@@ -1,6 +1,18 @@
+import numpy as np
+
 def objective(x):
   return x**2
 
 x = 4.0
-result= objective(x)
-print('f(%.3f) = %.3f' % (x, result))
+
+# define range for input
+r_min, r_max = -5.0, 5.0
+# sample input range uniformly at 0.1 increments
+inputs = np.arange(r_min, r_max, 0.1)
+# compute targets
+results = objective(inputs)
+
+# create a mapping of inputs to some results
+print('A few results:')
+for i in range(5):
+  print('f(%.3f) = %.3f' % (inputs[i], results[i]))
